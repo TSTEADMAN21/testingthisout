2d2983ffbe2dac65819fe76574373ddadddd089a
TSTEADMAN21 <tstead33@gmail.com>
2016-04-11 13:36:46 -0500

    Added files via upload

---
+++ youtube tutorial - Copy/js/_vti_cnf/jquery.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|STEADMAN\\travis
+vti_modifiedby:SR|STEADMAN\\travis
+vti_timelastmodified:TR|29 Feb 2016 20:45:44 -0000
+vti_timecreated:TR|29 Feb 2016 17:28:39 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Product.html services.html contact.html main.dwt about.html slider.dwt main.html index.html
+vti_nexttolasttimemodified:TW|29 Feb 2016 20:45:03 -0000
+vti_cacheddtm:TX|29 Feb 2016 20:45:44 -0000
+vti_filesize:IR|920
---
+++ youtube tutorial - Copy/js/jquery.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+
+$(function(){
+var width = 720;
+var animationSpeed = 1000;
+var pause = 3000;
+
+var currentSlide = 1;
+// cache DOM
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+var interval;
+
+function startSlider(){
+interval = setInterval(function(){
+	$slideContainer.animate({'margin-left':'-='+width},animationSpeed , function(){
+	
+		currentSlide++;
+		if(currentSlide === $slides.length){
+		currentSlide = 1;
+		$slideContainer.css('margin-left', 0);
+		}
+		});
+},pause);
+	}
+	
+	function stopSlider(){
+	clearInterval(interval);
+	}
+	
+	$slider.on('mouseenter',stopSlider).on('mouseleave',startSlider);
+	
+	// listen for mouse enter and pause
+	//resume on mouse leave
+	
+	startSlider();
+});
+
+
+// found video and source for slider https://www.youtube.com/watch?v=KkzVFB3Ba_o    : http://jsfiddle.net/EjZzs/15/
